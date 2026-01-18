d42e966f75bca639294ebb1a937cc62099a0f038
bickfordd-bit <bickfordd@gmail.com>
2026-01-18 20:25:44 +0000

    chore: finalize ledger package move and config sync

--- packages/web-ui/lib/ledger/index.ts
+++
@@ -1,6 +0,0 @@
-export const isEdge =
-  typeof process === "undefined" || process.env.NEXT_RUNTIME === "edge";
-
-export const ledger = isEdge
-  ? await import("./fs-ledger.edge")
-  : await import("./fs-ledger.node");
