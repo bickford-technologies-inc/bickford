101888301831e5edd0e33f0a0fc319de24c31e79
bickfordd-bit <bickfordd@gmail.com>
2026-01-18 22:12:53 +0000

    fix: ESM Node.js built-in imports, canon-closed, Vercel green

--- packages/ledger/src/fs-ledger.node.ts
+++ packages/ledger/src/fs-ledger.node.ts
@@ -1,5 +1,5 @@
-import fs from "fs/promises";
-import path from "path";
+import * as fs from "fs/promises";
+import * as path from "path";
 
 const LEDGER_ROOT = path.join(process.cwd(), ".ledger");
 
