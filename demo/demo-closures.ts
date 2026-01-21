9c48042b3086bf3de81c02fe12c15a946e894710
bickfordd-bit <bickfordd@gmail.com>
2026-01-21 14:24:42 -0500

    Add canon execution helpers

--- demo/demo-closures.ts
+++ demo/demo-closures.ts
@@ -24,12 +24,13 @@ import {
 } from "../packages/canon/src/canon/optr";
 
 import {
-  promotionGate,
   createExecutionContext,
   bufferTokensWithProof,
   sealChatItem,
   finalizeChatItem,
 } from "../packages/canon/src/canon";
+
+import { promotionGate } from "../packages/canon/src/canon/promotion";
 
 import {
   formatWhyNotPanel,
