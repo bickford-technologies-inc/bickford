9a5fb1912d983c02b8981f5ed713004950083a90
Bickford D <bickfordd@gmail.com>
2026-01-30 19:07:13 +0000

    fix: clean up component exports, remove unused BickfordChat, ensure all exports/imports are correct for React, prevent undefined component errors

--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,12 @@
 // This file ensures Next.js recognizes the /components directory for TypeScript
 export {};
+
+export { BickfordCommandDock } from "./BickfordCommandDock";
+export { DetailPanel } from "./DetailPanel";
+export { default as LedgerTable } from "./LedgerTable";
+export { LogsModal } from "./LogsModal";
+export { ModuleGrid } from "./ModuleGrid";
+export { ToastStack } from "./ToastStack";
+export { Sidebar } from "./Sidebar";
+export { default as NarrativeToggle } from "./NarrativeToggle";
+export { default as CompressionStats } from "./CompressionStats";
